4059da16c38fa5099e80971ac0f850438805866b
marvbo <marvinbogenschuetz@gmx.de>
2022-11-12 10:29:58 +0100

    added plot of measured distance of file

--- Messdaten/Auswertung_erste_Messung.m
+++ Messdaten/Auswertung_erste_Messung.m
@@ -1,6 +1,9 @@
 % Projekt Sensorsysteme
 % Auswertung erste Messungen 
 
-file =  fileread("MEssung30mm.TXT");
+file =  csvread("MEssung30mm.TXT",1);
 % Split the data at $
-data = regexp(file,',','split')
+
+
+plot(file(:,6),file(:,1))
+grid on
