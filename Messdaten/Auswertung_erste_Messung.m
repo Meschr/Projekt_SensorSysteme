51cddaaa842d56b2987fe04299a505018b15b91c
Meschr <marcel-escher@gmx.de>
2022-11-15 21:22:20 +0100

    added accelerometer plot

--- Messdaten/Auswertung_erste_Messung.m
+++ Messdaten/Auswertung_erste_Messung.m
@@ -1,9 +1,26 @@
 % Projekt Sensorsysteme
 % Auswertung erste Messungen 
+clc;
+clear;
 
-file =  csvread("MEssung30mm.TXT",1);
+file =  csvread("ZYX_hinUndHer.TXT",1);
 % Split the data at $
 
+positionSensor = file(:,1);
+t = linspace(0.002, 0.002*length(positionSensor), length(positionSensor))'; %Zeitvektor
+accelX = file(:,3);
+accelY = file(:,4);
+accelZ = file(:,5);
 
-plot(file(:,6),file(:,1))
+figure(1)
+plot(t,positionSensor);
 grid on
+
+figure(2)
+title("accelerometer plot");
+hold on;
+grid on;
+plot(t,accelX);
+plot(t,accelY);
+plot(t,accelZ);
+legend("X-Axis","Y-Axis","Z-Axis");
