b34ab7354ae49e84b83bd268304e53060d536007
theotis222 <120394340+theotis222@users.noreply.github.com>
2022-12-13 20:07:25 +0100

    added further measurements

--- Messdaten/Auswertung_erste_Messung.m
+++ Messdaten/Auswertung_erste_Messung.m
@@ -6,13 +6,15 @@ clc;
 clear all;
 close all;
 
-file =  importdata("Statisch_1.TXT", ',',2);
+g = 9.81;
+
+file =  importdata("Statisch_2.TXT", ',',2);
 % Split the data at $
 
 positionSensor = file.data(:,1);
 t = linspace(0.002, 0.002*length(positionSensor), length(positionSensor))'; %Zeitvektor
-accelX_1 = file.data(:,3)./2;      % Sensor unten   Due to offset (2G instead 1G we divide /2)
-accelX_2 = file.data(:,4)./2;      % Sensor oben    Due to offset (2G instead 1G we divide /2)
+accelX_1 = (file.data(:,3)./2);      % Sensor unten   Due to offset (2G instead 1G we divide /2)
+accelX_2 = (file.data(:,4)./2);      % Sensor oben    Due to offset (2G instead 1G we divide /2)
 
 % Average or mean value of array
 
@@ -28,27 +30,19 @@ mw_2 = mean(accelX_2)
 
 % Load next file
 
-file_1 =  importdata("Messung_1.TXT", ',',2);
+file_1 =  importdata("Messung_2_Zittrie.TXT", ',',2);
 % Split the data at $
 
-positionSensor_1 = file_1.data(:,1)./40;
+positionSensor_1 = file_1.data(:,1);
 t = linspace(0.002, 0.002*length(positionSensor_1), length(positionSensor_1))'; %Zeitvektor
-accelX_11 = file_1.data(:,3)./2;      % Sensor unten   Due to offset (2G instead 1G we divide /2)
-accelX_21 = file_1.data(:,4)./2;      % Sensor oben    Due to offset (2G instead 1G we divide /2)
+accelX_11 = (file_1.data(:,3)./2);      % Sensor unten   Due to offset (2G instead 1G we divide /2)
+accelX_21 = (file_1.data(:,4)./2);      % Sensor oben    Due to offset (2G instead 1G we divide /2)
 
 
 accelX_1_zero = accelX_11 - mw_1;
 accelX_2_zero = accelX_21 - mw_2;
 
-diff = accelX_2_zero - accelX_1_zero;
-
-figure(4)
-plot(t,diff)
-xlabel('time t in s');
-ylabel('distance in mm');
-grid on
-hold on 
-plot(t,positionSensor_1)
+diff = (accelX_1_zero - accelX_2_zero).*g ;
 
 figure(1)
 plot(t,positionSensor_1);
@@ -66,14 +60,39 @@ xlabel('time t in s');
 ylabel('acceleration in G');
 legend("X-Unten","X-Oben");
 
+figure(3)
+plot(t,diff)
+xlabel('time t in s');
+ylabel('distance in mm');
+grid on
+hold on 
+plot(t,positionSensor_1)
+
 
 windowSize = 5; 
 b = (1/windowSize)*ones(1,windowSize);
 a = 1;
 
 %% Verarbeitung (1.Glätten ...)
-filteredX =  filter(b,a,accelX_1);
-figure(3)
-hold on 
-plot(t,accelX_1);
-plot(t,filteredX);
+% filteredX =  filter(b,a,accelX_1);
+% figure(3)
+% hold on 
+% plot(t,accelX_1);
+% plot(t,filteredX);
+
+
+v = cumtrapz(diff);
+figure(4)
+plot(t,v)
+title('Integral 1 -> velocity')
+
+x = cumtrapz(v);
+figure(5)
+plot(t,x./1000)
+hold on
+plot(t,positionSensor_1)
+grid on
+legend('x -> acc','s -> ref')
+xlabel('time in t')
+ylabel('distance in mm')
+title('Integral 2 -> Distance')
